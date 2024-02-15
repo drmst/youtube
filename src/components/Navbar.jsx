2352221f4894475c86697dac450d6d87fdd7cdbf
Mesut <doktormesut@gmail.com>
2024-02-16 00:05:24 +0300

    lama.dev commerce

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,70 @@
+import { Search } from "@mui/icons-material";
+import React from "react";
+import styled from "styled-components";
+
+const Navbar = () => {
+  const Container = styled.div`
+    height: 60px;
+  `;
+
+  const Wrapper = styled.div`
+    padding: 10px 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  `;
+
+  const Left = styled.div`
+    flex: 1;
+    display: flex;
+    align-items: center;
+  `;
+
+  const Language = styled.span`
+    font-size: 14px;
+    cursor: pointer;
+  `;
+
+  const SearchContainer = styled.div`
+    border: 0.5px solid lightgray;
+    display: flex;
+    align-items: center;
+    margin-left: 25px;
+    padding: 5px;
+  `;
+
+const Input =styled.input`
+border: none;
+`
+
+  const Center = styled.div`
+    flex: 1;
+    text-align: center;
+  `;
+
+
+const Logo =styled.h1`
+font-weight: bold;
+`
+  const Right = styled.div`
+    flex: 1;
+  `;
+
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input/>
+            <Search />
+          </SearchContainer>
+        </Left>
+        <Center><Logo>LAMA.</Logo></Center>
+        <Right>Right</Right>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Navbar;
